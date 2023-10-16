a1712e4a4747f477b3778863bcfc1f2557d2182e
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-16 15:26:57 +0300

    Update fr_dimon.py

--- fr_dimon.py
+++ fr_dimon.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 from streamlit_js_eval import streamlit_js_eval
 import time
 st.set_page_config(layout="wide")
-html = """<iframe title="Рабочий день" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMGNlZGVlMGEtNDc3Mi00NDllLThjODEtMWFkMTEwNjhlNzgxIiwidCI6IjdjZTQ0MmYwLTExNTAtNGRjYS1iYzBiLWIxMTgwMzdiZTk1NSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>""", unsafe_allow_html=True
+html = """<iframe title="Рабочий день" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMGNlZGVlMGEtNDc3Mi00NDllLThjODEtMWFkMTEwNjhlNzgxIiwidCI6IjdjZTQ0MmYwLTExNTAtNGRjYS1iYzBiLWIxMTgwMzdiZTk1NSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>"""
 components.html(html, height=1000)
 a = st.empty()
 c_time = time.time()
