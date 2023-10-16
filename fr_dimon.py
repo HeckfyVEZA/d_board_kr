8bf0f6a0f33078083b7695659b985527242a1c6e
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-16 13:51:17 +0300

    Update fr_dimon.py

--- fr_dimon.py
+++ fr_dimon.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 from streamlit_js_eval import streamlit_js_eval
 import time
 st.set_page_config(layout="wide")
-components.iframe("""https://app.powerbi.com/view?r=eyJrIjoiMGNlZGVlMGEtNDc3Mi00NDllLThjODEtMWFkMTEwNjhlNzgxIiwidCI6IjdjZTQ0MmYwLTExNTAtNGRjYS1iYzBiLWIxMTgwMzdiZTk1NSIsImMiOjl9"""", width=1920, height=1080, scrolling=True)
+components.iframe("""https://app.powerbi.com/view?r=eyJrIjoiMGNlZGVlMGEtNDc3Mi00NDllLThjODEtMWFkMTEwNjhlNzgxIiwidCI6IjdjZTQ0MmYwLTExNTAtNGRjYS1iYzBiLWIxMTgwMzdiZTk1NSIsImMiOjl9""", width="100%", height="100%", scrolling=True)
 a = st.empty()
 c_time = time.time()
 while True:
